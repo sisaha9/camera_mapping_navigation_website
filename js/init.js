0875046c7f1a095baca6e94566d4224b12afca07
sisaha9 <sisaha@ucsd.edu>
2021-03-05 21:06:11 -0800

    Adding init.js changes

--- js/init.js
+++ js/init.js
@@ -9,16 +9,6 @@
 
 $(".dropdown-trigger").dropdown();
 
-// document.addEventListener('DOMContentLoaded', function() {
-// var elems = document.querySelectorAll('.collapsible');
-// var instances = M.Collapsible.init(elems, options);
-// });
-
-// Or with jQuery
-
-// $(document).ready(function(){
-// $('.collapsible').collapsible();
-// });
 var content = {
 	"nav_hidden_content1": "The data is collected using a ROS utility called <a href=http://wiki.ros.org/rosbag>rosbag</a>. Since the camera outputs images at 60FPS we throttle it down with the help of <a href=http://wiki.ros.org/topic_tools/throttle>topic_tools throttle</a> ROS package. This basically diverts the camera stream into a new topic that only outputs at a frequency of 1Hz. Once this is setup we run the throttle package to divert the input and use rosbag to record the diverted input giving us unique images across the track and reducing file size taken by a given lap. The images are then extracted from this bag file using the <a href=http://wiki.ros.org/rosbag/Tutorials/Exporting%20image%20and%20video%20data>image_view</a> ROS package. Once converted into images we now have a part of our dataset ready",
 	"nav_hidden_content2": "The dataset is labelled in the COCO JSON format with the help of <a href=https://www.makesense.ai/>MakeSenseAI</a>. We labelled around 2 laps of data. Since we ran a model that was pretrained on the COCO dataset we believed it would be sufficient. The images were cropped so that the top half is not visible during training and inference. This reduced a lot of the noise from the background",
